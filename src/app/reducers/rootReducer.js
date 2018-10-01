d57fdbdc66039462c24784d94d922da648b3148d
Ryan Murry <ryanjmurry@gmail.com>
2018-10-01 13:31:17 -0700

    add redux form reducer

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import modalReducer from '../../features/modals/modalReducer';
 import authReducer from '../../features/auth/authReducer';
+import { reducer as FormReducer } from 'redux-form'; 
 import { firebaseReducer } from 'react-redux-firebase'
 import { firestoreReducer } from 'redux-firestore'
 
 const rootReducer = combineReducers({
+  form: FormReducer,
   modals: modalReducer,
   firebase: firebaseReducer,
   firestore: firestoreReducer,
