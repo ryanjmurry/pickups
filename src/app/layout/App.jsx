f69285ff837e81a7d711956e311e44e951579ba9
Ryan Murry <ryanjmurry@gmail.com>
2018-10-01 09:16:35 -0700

    initial commit with anticipated file structure

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        App Works
+      </div>
+    );
+  }
+}
+
+export default App;
