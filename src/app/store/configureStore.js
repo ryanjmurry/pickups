667185c9800f9fbab40bbef35b789f5879256c3f
Ryan Murry <ryanjmurry@gmail.com>
2018-10-01 10:39:14 -0700

    enable HMR for reducers

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -11,5 +11,15 @@ export const configureStore = preloadedState => {
   const composedEnhancer = composeWithDevTools(...storeEnhancers);
 
   const store = createStore(rootReducer, preloadedState, composedEnhancer);
+
+  if (process.env.NODE_ENV !== 'production') {
+    if (module.hot) {
+      module.hot.accept('../reducers/rootReducer', () => {
+        const newRootReducer = require('../reducers/rootReducer').default;
+        store.replaceReducer(newRootReducer);
+      });
+    }
+  }
+
   return store;
 };
