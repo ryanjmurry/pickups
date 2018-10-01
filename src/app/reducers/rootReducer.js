9c43e190c8d9a550959a3a4cf72746b39f7e2dfe
Ryan Murry <ryanjmurry@gmail.com>
2018-10-01 11:50:49 -0700

    add firebase and firestore built in reducers

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import modalReducer from '../../features/modals/modalReducer';
+import { firebaseReducer } from 'react-redux-firebase'
+import { firestoreReducer } from 'redux-firestore'
 
 const rootReducer = combineReducers({
-  modals: modalReducer
+  modals: modalReducer,
+  firebase: firebaseReducer,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
