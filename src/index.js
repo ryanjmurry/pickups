ba3c3be5c9aae990c954d9108cdf734894f0000b
Ryan Murry <ryanjmurry@gmail.com>
2018-10-01 10:36:00 -0700

    import provider and configure store and give our application access to redux store

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { configureStore } from './app/store/configureStore';
+
+const store = configureStore();
 
 const rootEl = document.getElementById('root');
 
 let render = () => {
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
     rootEl
   );
 };
