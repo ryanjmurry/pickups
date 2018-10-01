0dfbb11ad10f2e028031f6414fe6b1fe38bcb8cb
Ryan Murry <ryanjmurry@gmail.com>
2018-10-01 10:42:31 -0700

    fix type

--- src/app/common/util/reducerUtil.js
+++ src/app/common/util/reducerUtil.js
@@ -1,5 +1,5 @@
 export const createReducer = (initialState, fnMap) => {
-  return (state = initialState, { type, payloar }) => {
+  return (state = initialState, { type, payload }) => {
     const handler = fnMap[type];
 
     return handler ? handler(state, payload) : state;
