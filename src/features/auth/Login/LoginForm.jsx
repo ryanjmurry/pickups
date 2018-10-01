15c0a0b3f9e113494b1476d9e7026c1e0929e99e
Ryan Murry <ryanjmurry@gmail.com>
2018-10-01 14:17:20 -0700

    display an error label if the login fails

--- src/features/auth/Login/LoginForm.jsx
+++ src/features/auth/Login/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
-import { Form, Segment, Button } from 'semantic-ui-react';
+import { Form, Segment, Button, Label } from 'semantic-ui-react';
 import TextInput from '../../../app/common/form/TextInput';
 import { login } from '../authActions';
 
@@ -9,15 +9,20 @@ const actions = {
   login
 };
 
-const LoginForm = ({ login, handleSubmit }) => {
+const LoginForm = ({ login, handleSubmit, error }) => {
   return (
     <Form onSubmit={handleSubmit(login)}>
       <Segment>
         <Field name="email" type="text" component={TextInput} placeholder="Email Address" />
-        <Field name='password' type='password' component={TextInput} placeholder='Password' />
-        <Button color='green'>
-          Login
-        </Button>
+        <Field name="password" type="password" component={TextInput} placeholder="Password" />
+        <div>
+          {error && (
+            <Label basic color="red">
+              {error}
+            </Label>
+          )}
+        </div>
+        <Button color="green">Login</Button>
       </Segment>
     </Form>
   );
