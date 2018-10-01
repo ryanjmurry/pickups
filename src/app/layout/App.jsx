a16dec9734e6cb0dc7dd95ee2d18bcb1ad9a308d
Ryan Murry <ryanjmurry@gmail.com>
2018-10-01 09:40:29 -0700

    import switch and route from react router. test routing works on splash page and pickup dashboard page

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import SplashPage from '../../features/splash/SplashPage';
+import PickUpDashboard from '../../features/pickup/PickUpDashboard/PickUpDashboard';
 class App extends Component {
   render() {
     return (
       <div>
-        App Works
+        <Switch>
+          <Route exact path='/' component={SplashPage} />
+          <Route path='/pickups' component={PickUpDashboard} />
+        </Switch>
       </div>
     );
   }
