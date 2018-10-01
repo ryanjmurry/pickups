7003e17ea91aa7da38162fd2cc29717953cc44cc
Ryan Murry <ryanjmurry@gmail.com>
2018-10-01 15:44:05 -0700

    create social login component that will hold any future social logins

---
+++ src/features/auth/SocialLogin/SocialLogin.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button, Icon } from 'semantic-ui-react';
+
+const SocialLogin = ({  }) => {
+  return (
+    <div>
+      <Button type="button" color='google plus'>
+        <Icon name='google plus'/>
+        Login with Google!
+      </Button>
+    </div>
+  )
+}
+
+export default SocialLogin
