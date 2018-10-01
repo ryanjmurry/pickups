eb5d354db6afc5dcd4d5fdf2525fd546e79f9e71
Ryan Murry <ryanjmurry@gmail.com>
2018-10-01 09:43:01 -0700

    import semantic ui container and add to app

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import SplashPage from '../../features/splash/SplashPage';
 import PickUpDashboard from '../../features/pickup/PickUpDashboard/PickUpDashboard';
+import { Container } from 'semantic-ui-react';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <Switch>
-          <Route exact path='/' component={SplashPage} />
-          <Route path='/pickups' component={PickUpDashboard} />
-        </Switch>
+        <Container>
+          <Switch>
+            <Route exact path="/" component={SplashPage} />
+            <Route path="/pickups" component={PickUpDashboard} />
+          </Switch>
+        </Container>
       </div>
     );
   }
