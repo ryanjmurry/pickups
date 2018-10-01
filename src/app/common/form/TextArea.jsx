2cd906a91abd4e8bc8830707ba51292f9f8ed413
Ryan Murry <ryanjmurry@gmail.com>
2018-10-01 09:51:24 -0700

    create reusable text area field for forms

---
+++ src/app/common/form/TextArea.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Form, Label } from 'semantic-ui-react';
+
+const TextInput = ({ input, rows, placeholder, meta: { touched, error} }) => {
+  return (
+    <Form.Field error={touched && !!error}>
+      <input {...input} placeholder={placeholder} rows={rows}/>
+      {touched &&
+        error && (
+          <Label basic color="red">
+            {error}
+          </Label>
+        )}
+    </Form.Field>
+  )
+}
+
+export default TextInput
