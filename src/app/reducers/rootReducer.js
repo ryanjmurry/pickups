3f74f464069758067f3772ca70f1abd9c89c5e62
Ryan Murry <ryanjmurry@gmail.com>
2018-10-01 12:59:43 -0700

    add auth reducer to root reducer

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import modalReducer from '../../features/modals/modalReducer';
+import authReducer from '../../features/auth/authReducer';
 import { firebaseReducer } from 'react-redux-firebase'
 import { firestoreReducer } from 'redux-firestore'
 
 const rootReducer = combineReducers({
   modals: modalReducer,
   firebase: firebaseReducer,
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
+  auth: authReducer
 });
 
 export default rootReducer;
