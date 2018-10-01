c15437d487ed8218af14ba3dd67884e03af9467e
Ryan Murry <ryanjmurry@gmail.com>
2018-10-01 15:14:57 -0700

    create sign up form. imports sign up action from auth actions and calls method on form submit through redux form method handleSubmit(). Displays any errors after user clicks submit

---
+++ src/features/auth/SignUp/SignUpForm.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import { Form, Segment, Button, Label } from 'semantic-ui-react';
+import TextInput from '../../../app/common/form/TextInput';
+import { signUpUser } from '../authActions';
+
+const actions = {
+  signUpUser
+};
+
+const SignUpForm = ({ signUpUser, handleSubmit, error }) => {
+  return (
+    <Form onSubmit={handleSubmit(signUpUser)}>
+      <Segment>
+        <Field name="name" type="text" component={TextInput} placeholder="Display Name" />
+        <Field name="email" type="text" component={TextInput} placeholder="Email Address" />
+        <Field name="password" type="password" component={TextInput} placeholder="Password" />
+        <div>
+          {error && (
+            <Label basic color="red">
+              {error}
+            </Label>
+          )}
+        </div>
+        <Button color="green">Game On!</Button>
+      </Segment>
+    </Form>
+  );
+};
+
+export default connect(
+  null,
+  actions
+)(reduxForm({ form: 'signUpForm' })(SignUpForm));
