88479b7309432a7b1cdd50324f29af59dbe60ed8
Ryan Murry <ryanjmurry@gmail.com>
2018-10-01 13:31:05 -0700

    add form components from semantic ui react, connect login action from auth actions, bring in handleSubmit prop from redux forms, handle login action on form submit

--- src/features/auth/Login/LoginForm.jsx
+++ src/features/auth/Login/LoginForm.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Form, Segment, Button } from 'semantic-ui-react';
 import { Field, reduxForm } from 'redux-form';
+import { Form, Segment, Button } from 'semantic-ui-react';
 import TextInput from '../../../app/common/form/TextInput';
-import { login, logout } from '../authActions';
+import { login } from '../authActions';
 
 const actions = {
-  login,
-  logout
-}
-
-const LoginForm = ({ login, logout }) => {
-  return <div />;
+  login
 };
 
-export default connect(null, actions)(reduxForm({ form: 'loginForm' })(LoginForm));
+const LoginForm = ({ login, handleSubmit }) => {
+  return (
+    <Form onSubmit={handleSubmit(login)}>
+      <Segment>
+        <Field name="email" type="text" component={TextInput} placeholder="Email Address" />
+        <Field name='password' type='password' component={TextInput} placeholder='Password' />
+        <Button color='green'>
+          Login
+        </Button>
+      </Segment>
+    </Form>
+  );
+};
+
+export default connect(
+  null,
+  actions
+)(reduxForm({ form: 'loginForm' })(LoginForm));
