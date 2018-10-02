142977c66dbd07eb92f07e358442f180bf5264d3
Ryan Murry <ryanjmurry@gmail.com>
2018-10-01 23:00:32 -0700

    create reusable date input component. import react-datepicker & moment

---
+++ src/app/common/form/DateInput.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Form, Label } from 'semantic-ui-react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
+
+const DateInput = ({
+  input: { value, onChange, onBlur, ...restInput },
+  width,
+  placeholder,
+  meta: { touched, error },
+  ...rest
+}) => {
+  if (value) {
+    // parsing value using moment and formatting to a unix timestamp uniformity
+    value = moment(value, 'X');
+  }
+  return (
+    <Form.Field error={touched && !!error} width={width}>
+      <DatePicker
+        {...rest}
+        placeholderText={placeholder}
+        selected={value ? moment(value) : null} //check if date is already selected to repopulate on update
+        onChange={onChange}
+        onBlur={() => onBlur()}
+        {...restInput}
+      />
+      {touched &&
+        error && (
+          <Label basic color="red">
+            {error}
+          </Label>
+        )}
+    </Form.Field>
+  );
+};
+
+export default DateInput;
