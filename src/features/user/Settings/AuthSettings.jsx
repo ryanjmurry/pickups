fbc7a72c737e3f0098881f5032efd9fbf139a8b3
Ryan Murry <ryanjmurry@gmail.com>
2018-10-02 14:52:07 -0700

    add redux form to handle changing password

--- src/features/user/Settings/AuthSettings.jsx
+++ src/features/user/Settings/AuthSettings.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Form, Button } from 'semantic-ui-react';
+import TextInput from '../../../app/common/form/TextInput';
 
 const AuthSettings = () => {
   return (
-    <div>
-      <h1>Auth Settings</h1>
-    </div>
-  )
-}
+    <Form>
+        <Field name="newPassword1" type="password" component={TextInput} placeholder="Enter your new password" />
+        <Field name="newPassword2" type="password" component={TextInput} placeholder="Confirm new password" />
+    </Form>
+  );
+};
 
-export default AuthSettings
+export default reduxForm({ form: 'authSettingsForm' })(AuthSettings);
+
+//if email provider, provide way to update email
+// if google provider, provide link to google account to update there
