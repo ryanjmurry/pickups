8d1e154f7cfe11d95b0a192bb9e9e85641bfcd85
Ryan Murry <ryanjmurry@gmail.com>
2018-10-01 18:08:57 -0700

    remove api key, delete and generate new firebase project, import firebase config from gitignored file

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -1,14 +1,6 @@
 import firebase from 'firebase';
 import 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAqnjCew1FrGIbKBK_bu8qzCc1G5G2tF8k",
-  authDomain: "pdx-pickups.firebaseapp.com",
-  databaseURL: "https://pdx-pickups.firebaseio.com",
-  projectId: "pdx-pickups",
-  storageBucket: "pdx-pickups.appspot.com",
-  messagingSenderId: "17255737028"
-}
+import firebaseConfig from './firebaseConfig';
 
 firebase.initializeApp(firebaseConfig);
 firebase.firestore();
