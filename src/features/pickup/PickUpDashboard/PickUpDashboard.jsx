737b746ead22f7e39cd5aec09fa2e9629a9e4fce
Ryan Murry <ryanjmurry@gmail.com>
2018-10-02 03:27:39 -0700

    connect dashboard to redux and firestore, bring in list of ordered pickups, destrucrure, and pass to pickup list

--- src/features/pickup/PickUpDashboard/PickUpDashboard.jsx
+++ src/features/pickup/PickUpDashboard/PickUpDashboard.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react';
+import PickupList from '../PickupList/PickupList';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
 
-const PickUpDashboard = () => {
-  return (
-    <div>
-      <h1>PickUp Dashboard</h1>
-    </div>
-  )
+const mapState = state => ({
+  pickups: state.firestore.ordered.pickups
+})
+
+class PickupDashboard extends Component {
+  render() {
+    const { pickups } = this.props;
+    return (
+      <div>
+        <h1>PickUp Dashboard</h1>
+        <PickupList pickups={pickups}/>
+      </div>
+    );
+  }
 }
 
-export default PickUpDashboard
+export default connect(mapState)(firestoreConnect([{ collection: 'pickups' }])(PickupDashboard));
