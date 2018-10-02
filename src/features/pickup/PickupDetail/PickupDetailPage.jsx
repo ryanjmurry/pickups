0a281af611bc2f9caea37b14d273a8685e6fa1ff
Ryan Murry <ryanjmurry@gmail.com>
2018-10-02 09:59:41 -0700

    connect component to firestore through HOC, use firestore get method to retrieve specific event matching url param, set pickup to found pickup

---
+++ src/features/pickup/PickupDetail/PickupDetailPage.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withFirestore } from 'react-redux-firebase';
+import { Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+const mapState = state => {
+  let pickup = {};
+  if (state.firestore.ordered.pickups && state.firestore.ordered.pickups[0]) {
+    pickup = state.firestore.ordered.pickups[0];
+  }
+  return {
+    pickup
+  };
+};
+
+class PickupDetailPage extends Component {
+  async componentDidMount() {
+    const { firestore, match, history } = this.props;
+    let pickup = await firestore.get(`pickups/${match.params.id}`);
+    if (!pickup.exists) {
+      history.push('/events');
+    }
+  }
+  render() {
+    const { pickup } = this.props;
+    return (
+      <div>
+        <h1>Pickup Detail</h1>
+        <Button as={Link} to={`/edit/${pickup.id}`} content='Edit Event'/>
+      </div>
+    );
+  }
+}
+
+export default withFirestore(connect(mapState)(PickupDetailPage));
