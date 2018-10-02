cc90071fbd4e5143be310115780d7157c5774a87
Ryan Murry <ryanjmurry@gmail.com>
2018-10-02 00:28:11 -0700

    create form to create pick up game. import reusuable form input components

--- src/features/pickup/PickUpForm/PickUpForm.jsx
+++ src/features/pickup/PickUpForm/PickUpForm.jsx
@@ -1,9 +1,67 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Form, Button, Label } from 'semantic-ui-react';
+import TextInput from '../../../app/common/form/TextInput';
+import TextArea from '../../../app/common/form/TextArea';
+import SelectInput from '../../../app/common/form/SelectInput';
+import DateInput from '../../../app/common/form/DateInput';
 
-class PickUpForm extends Component {
+const skillLevel = [
+  { key: 'rookie', text: 'Rookie', value: 'rookie' },
+  { key: 'pro', text: 'Pro', value: 'pro' },
+  { key: 'allstar', text: 'All-Star', value: 'allstar' },
+  { key: 'superstar', text: 'Superstar', value: 'superstar' }
+];
+
+class PickupForm extends Component {
   render() {
-    return <div />;
+    return (
+      <Form>
+        <Field name="title" type="text" component={TextInput} placeholder="Title your pickup!" />
+        <Field
+          name="level"
+          type="text"
+          component={SelectInput}
+          options={skillLevel}
+          placeholder="What's the desired skill level of players?"
+        />
+        <Field
+          name="capacity"
+          type="number"
+          min={2}
+          component={TextInput}
+          placeholder="How many players is too many?"
+        />
+        <Field
+          name="description"
+          type="text"
+          component={TextArea}
+          placeholder="Tell us about the pickup!"
+        />
+        <Field name="city" type="text" component={TextInput} placeholder="Pickup city?" />
+        <Field
+          name="address"
+          type="text"
+          component={TextInput}
+          placeholder="Enter the pickup address"
+        />
+        <Field name="venue" type="text" component={TextInput} placeholder="Enter the venue name" />
+        <Field
+          name="date"
+          type="text"
+          component={DateInput}
+          placeholder="Date and time?"
+          dateFormat="YYYY-MM-DD HH:mm"
+          timeFormat="HH:mm"
+          timeCaption="time"
+          showTimeSelect
+        />
+        <Button color='teal'>
+          Create Pickup!
+        </Button>
+      </Form>
+    );
   }
 }
 
-export default PickUpForm;
+export default reduxForm({ form: 'pickupForm' })(PickupForm);
