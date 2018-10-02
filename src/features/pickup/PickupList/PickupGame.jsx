7b2cffd2e74bebda1fefd3a728637092f7651e7c
Ryan Murry <ryanjmurry@gmail.com>
2018-10-02 10:00:05 -0700

    add button to direct user to pickup edit form through its id

--- src/features/pickup/PickupList/PickupGame.jsx
+++ src/features/pickup/PickupList/PickupGame.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const PickupGame = ({ pickup }) => {
   return (
     <div>
-      <h1>PickupGame</h1>
-      {pickup.address}
+      <h3>{pickup.title}</h3>
+      <Button as={Link} to={`/pickup/${pickup.id}`} content="View" />
     </div>
   );
 };
