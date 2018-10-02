04812491db450c1970d52b007fcd4d3b2937d4c9
Ryan Murry <ryanjmurry@gmail.com>
2018-10-02 16:27:00 -0700

    connect redux store, import router, switch, redirect from react-router, redirect /settings to /settings/general, mapState to props and pass down to setting dashboard subcomponents

--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React from 'react';
 import SettingsNav from './SettingsNav';
 import AuthSettings from './AuthSettings';
 import GeneralSettings from './GeneralSettings';
+import { connect } from 'react-redux';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
-const SettingsDashboard = () => {
+const mapState = state => ({
+  providerId: state.firebase.auth.providerData[0].providerId,
+  user: state.firebase.profile
+});
+
+const SettingsDashboard = ({ providerId, user }) => {
   return (
     <div>
       <h1>Settings Dash</h1>
-      <AuthSettings />
-      <GeneralSettings />
-      <SettingsNav />
+      <Switch>
+        <Redirect exact from="/settings" to="/settings/general" />
+        <Route path="/settings/auth" render={() => <AuthSettings providerId={providerId} />} />
+        <Route path="/settings/general" render={() => <GeneralSettings initialValues={user}/>} />
+      </Switch>
     </div>
-  )
-}
+  );
+};
 
-export default SettingsDashboard
+export default connect(mapState)(SettingsDashboard);
