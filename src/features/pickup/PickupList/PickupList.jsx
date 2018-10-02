ec35cd694d6c727473ec2894a24b0aae8ed777fe
Ryan Murry <ryanjmurry@gmail.com>
2018-10-02 03:28:08 -0700

    map through pickup list and render pickup game for each. check if actual pickups exist before attemping map function

---
+++ src/features/pickup/PickupList/PickupList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PickupGame from './PickupGame';
+
+const PickupList = ({ pickups }) => {
+  return (
+    <div>
+      <h1>Pick up List</h1>
+      {pickups && pickups.map(pickup => <PickupGame key={pickup.id} pickup={pickup}/>)}
+    </div>
+  );
+};
+
+export default PickupList;
