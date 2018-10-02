e976aeb5bfb0baea7968e5280a03acd9bba8a321
Ryan Murry <ryanjmurry@gmail.com>
2018-10-02 01:25:54 -0700

    create helper method to add additional initial data to new pickup submission.

---
+++ src/app/common/util/helpers.js
@@ -0,0 +1,22 @@
+import moment from 'moment';
+
+// destructures pickup received from PickupForm and adds additional information to it as it is initially created
+export const createNewPickup = (user, photoURL, pickup) => {
+  pickup.date = moment(pickup.date).toDate();
+  return {
+    ...pickup,
+    hostUid: user.uid,
+    hostedBy: user.displayName,
+    hostPhotoURL: photoURL || "/assets/user.png",
+    created: Date.now(),
+    attendees: [
+      [user.uid]: {
+        going: true,
+        joinDate: Date.now(),
+        photoURL: photoURL || "/assets/user.png",
+        displayName: user.displayName,
+        host: true
+      }
+    ]
+  }
+}
