f7e9f4603b2f0960dc747a0f42d0e0d7ae767a82
Ryan Murry <ryanjmurry@gmail.com>
2018-10-02 19:31:47 -0700

    expand on general settings form, add redux form configurations

--- src/features/user/Settings/GeneralSettings.jsx
+++ src/features/user/Settings/GeneralSettings.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { Form } from 'semantic-ui-react';
+import { Form, Button } from 'semantic-ui-react';
 import TextInput from '../../../app/common/form/TextInput';
 import DateInput from '../../../app/common/form/DateInput';
 import TextArea from '../../../app/common/form/TextArea';
-import SelectInput from '../../../app/common/form/SelectInput';
 
 const GeneralSettings = () => {
   return (
     <Form>
       <h1>General Settings</h1>
-      <Field name='displayName' component={TextInput}/>
-      <Field name='dateOfBirth' component={DateInput}/>
-      <Field name='city' component={TextInput}/>
-      <Field name='about' component={TextArea}/>
-      <Field name='interests' component={SelectInput}/>
+      <Field name="displayName" component={TextInput} placeholder="What name do you go by?" />
+      <Field
+        name="dateOfBirth"
+        component={DateInput}
+        width={8}
+        placeholder="When's your birthday?"
+        dateFormat="YYYY-MM-DD"
+        showYearDropdown={true}
+        showMonthDropdown={true}
+        dropdownMode="select"
+      />
+      <Field name="city" component={TextInput} placeholder="What is your city of residence?" />
+      <Field
+        name="about"
+        component={TextArea}
+        rows={2}
+        placeholder="Tell us a little bit about yourself!"
+      />
+      <Button content="Update Profile!" />
     </Form>
   );
 };
 
-export default reduxForm({ form: 'aboutForm' })(GeneralSettings);
+export default reduxForm({ form: 'aboutForm', enableReinitialize: true, destroyOnUnmount: false })(
+  GeneralSettings
+);
