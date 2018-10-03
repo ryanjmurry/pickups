67cdd4ea4adb0334b0a5e7a0a422f3987e34f989
Ryan Murry <ryanjmurry@gmail.com>
2018-10-02 19:40:41 -0700

    map actions and pass update profile action to general settings page

--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import SettingsNav from './SettingsNav';
+// import SettingsNav from './SettingsNav';
 import AuthSettings from './AuthSettings';
 import GeneralSettings from './GeneralSettings';
 import { connect } from 'react-redux';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { updateProfile } from '../userActions';
 
 const mapState = state => ({
   providerId: state.firebase.auth.providerData[0].providerId,
   user: state.firebase.profile
 });
 
-const SettingsDashboard = ({ providerId, user }) => {
+const actions = {
+  updateProfile
+}
+
+const SettingsDashboard = ({ providerId, user, updateProfile }) => {
   return (
     <div>
       <h1>Settings Dash</h1>
       <Switch>
         <Redirect exact from="/settings" to="/settings/general" />
         <Route path="/settings/auth" render={() => <AuthSettings providerId={providerId} />} />
-        <Route path="/settings/general" render={() => <GeneralSettings initialValues={user}/>} />
+        <Route path="/settings/general" render={() => <GeneralSettings initialValues={user} updateProfile={updateProfile}/>} />
       </Switch>
     </div>
   );
 };
 
-export default connect(mapState)(SettingsDashboard);
+export default connect(mapState, actions)(SettingsDashboard);
