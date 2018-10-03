94dc7f7ef6a6e30b19d7fc8b16ff8e1fd8b9973b
Ryan Murry <ryanjmurry@gmail.com>
2018-10-03 15:32:52 -0700

    move withFirebase and logout function to NavBar, clean up render and routes

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { withFirebase } from 'react-redux-firebase';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import PickupDashboard from '../../features/pickup/PickupDashboard/PickupDashboard';
 import LoginForm from '../../features/auth/Login/LoginForm';
-import { Container, Button } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import SignUpForm from '../../features/auth/SignUp/SignUpForm';
 import NavBar from '../../features/nav/NavBar/NavBar';
 import PickupForm from '../../features/pickup/PickupForm/PickupForm';
@@ -12,33 +11,23 @@ import PickupDetailPage from '../../features/pickup/PickupDetail/PickupDetailPag
 import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
 
 class App extends Component {
-  handleSignOut = () => {
-    this.props.firebase.logout();
-  };
   render() {
     return (
       <div>
         <NavBar />
-        <Container>
-          <br />
-          <br />
-          <br />
-          <br />
-          <Button onClick={this.handleSignOut}>Logout of Firebase</Button>
           <Switch>
             <Route exact path="/" component={HomePage} />
-            <Route path="/pickups" component={PickupDashboard} />
-            <Route path="/newpickup" component={PickupForm} />
             <Route path="/login" component={LoginForm} />
             <Route path="/signup" component={SignUpForm} />
+            <Route path="/pickups" component={PickupDashboard} />
+            <Route path="/newpickup" component={PickupForm} />
             <Route path="/edit/:id" component={PickupForm} />
             <Route path="/pickup/:id" component={PickupDetailPage} />
             <Route path='/settings' component={SettingsDashboard} />
           </Switch>
-        </Container>
       </div>
     );
   }
 }
 
-export default withFirebase(App);
+export default App;
