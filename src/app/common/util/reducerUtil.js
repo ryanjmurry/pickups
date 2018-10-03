aeb88a478cbb827af317915dde656e095515cd45
Ryan Murry <ryanjmurry@gmail.com>
2018-10-02 23:18:14 -0700

    remove unused file

--- src/app/common/util/reducerUtil.js
+++
@@ -1,8 +0,0 @@
-// redux documentation on reducing boilerplate code
-export const createReducer = (initialState, lookupMap) => {
-  return (state = initialState, { type, payload }) => {
-    const handler = lookupMap[type];
-
-    return handler ? handler(state, payload) : state;
-  };
-};
