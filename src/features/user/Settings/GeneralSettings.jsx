1f26741dbcafd72a8e4d250aa9d6f2a090835c47
Ryan Murry <ryanjmurry@gmail.com>
2018-10-02 18:52:35 -0700

    add form, import reusuable input components, hook form to redux forms

--- src/features/user/Settings/GeneralSettings.jsx
+++ src/features/user/Settings/GeneralSettings.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Form } from 'semantic-ui-react';
+import TextInput from '../../../app/common/form/TextInput';
+import DateInput from '../../../app/common/form/DateInput';
+import TextArea from '../../../app/common/form/TextArea';
+import SelectInput from '../../../app/common/form/SelectInput';
 
 const GeneralSettings = () => {
   return (
-    <div>
+    <Form>
       <h1>General Settings</h1>
-    </div>
-  )
-}
+      <Field name='displayName' component={TextInput}/>
+      <Field name='dateOfBirth' component={DateInput}/>
+      <Field name='city' component={TextInput}/>
+      <Field name='about' component={TextArea}/>
+      <Field name='interests' component={SelectInput}/>
+    </Form>
+  );
+};
 
-export default GeneralSettings
+export default reduxForm({ form: 'aboutForm' })(GeneralSettings);
