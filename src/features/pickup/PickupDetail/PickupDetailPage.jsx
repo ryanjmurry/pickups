af05feedb1d89a5b4d33c10955c924e19e28b8a7
Ryan Murry <ryanjmurry@gmail.com>
2018-10-03 08:52:35 -0700

    import pickup registration action, create buttons to join and leave game, handle register action on join button click

--- src/features/pickup/PickupDetail/PickupDetailPage.jsx
+++ src/features/pickup/PickupDetail/PickupDetailPage.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { withFirestore } from 'react-redux-firebase';
 import { Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import { registerForPickupGame } from '../../user/userActions';
 
 const mapState = state => {
   let pickup = {};
@@ -14,6 +15,10 @@ const mapState = state => {
   };
 };
 
+const actions = {
+  registerForPickupGame
+}
+
 class PickupDetailPage extends Component {
   async componentDidMount() {
     const { firestore, match, history } = this.props;
@@ -22,15 +27,18 @@ class PickupDetailPage extends Component {
       history.push('/events');
     }
   }
+
   render() {
-    const { pickup } = this.props;
+    const { pickup, registerForPickupGame } = this.props;
     return (
       <div>
         <h1>Pickup Detail</h1>
         <Button as={Link} to={`/edit/${pickup.id}`} content="Edit Event" />
+        <Button onClick={() => registerForPickupGame(pickup)} content="Join Pickup" />
+        <Button content="Leave Pickup" />
       </div>
     );
   }
 }
 
-export default withFirestore(connect(mapState)(PickupDetailPage));
+export default withFirestore(connect(mapState, actions)(PickupDetailPage));
