8a7405759b6da066020f8d49b9b39f28400383c6
Ryan Murry <ryanjmurry@gmail.com>
2018-10-02 21:39:49 -0700

    handle date formatting on date picker select

--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -1,6 +1,11 @@
+import moment from 'moment';
+
 export const updateProfile = user => {
   return async (dispatch, getState, { getFirebase }) => {
     const firebase = getFirebase();
+    if (user.dateOfBirth) {
+      user.dateOfBirth = moment(user.dateOfBirth).toDate();
+    }
     try {
       await firebase.updateProfile(user);
     } catch (error) {
