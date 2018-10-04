f47ceb77ea7eb3ff53b6b6f5a8db2f2f038d2824
Ryan Murry <ryanjmurry@gmail.com>
2018-10-04 05:27:58 -0700

    update pick up game component with mock data

--- src/features/pickup/PickupList/PickupGame.jsx
+++ src/features/pickup/PickupList/PickupGame.jsx
@@ -1,14 +1,56 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Segment, Grid, Progress, Header } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 const PickupGame = ({ pickup }) => {
   return (
-    <div>
-      <h3>{pickup.title}</h3>
-      <Button as={Link} to={`/pickup/${pickup.id}`} content="View" />
-    </div>
+    <Segment style={{ width: '700px', margin: 'auto' }}>
+      <Grid columns={3}>
+        <Grid.Row stretched>
+          <Grid.Column>
+            <Segment basic>
+              <Header style={{fontSize: '2.5em', marginBottom: '-2px'}}>title</Header>
+              <p style={{fontSize: '1.5em', marginBottom: '-2px'}}>datetime</p>
+              <p style={{fontSize: '1.2.5em'}}>city</p>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment basic />
+            <Segment basic>
+              <div style={{position: 'relative', top: '55px'}}>
+                <Button content="details" color="purple" fluid />
+              </div>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment textAlign="right" basic>
+              game type
+            </Segment>
+            <Segment basic />
+            <Segment basic>
+              <div style={{ position: 'relative', top: '20px' }}>
+                <Grid>
+                  <Grid.Row>
+                    <Grid.Column width={7}>
+                      <br />
+                      100% full
+                    </Grid.Column>
+                    <Grid.Column width={9}>
+                      capacity
+                      <Progress indicating size="small" />
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              </div>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
   );
 };
 
+// gamepad
+// basketball ball
+// chess
 export default PickupGame;
