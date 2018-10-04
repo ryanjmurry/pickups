b6aab78be6a6ba5b766f388b79c23d19514edb6b
Ryan Murry <ryanjmurry@gmail.com>
2018-10-03 18:42:40 -0700

    add slideshow to homepage

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Segment } from 'semantic-ui-react';
+import BackgroundSlideshow from 'react-background-slideshow';
+import arcade from '../../app/assets/arcade.jpg'
+import bball from '../../app/assets/bball.jpg';
+import boardGame from '../../app/assets/board-game.jpg';
+import bowling from '../../app/assets/bowling.jpg';
+import tugOfWar from '../../app/assets/tugofwar.jpg';
+import xbox from '../../app/assets/xbox.jpg';
+
+const slideShowStyle = {
+  height: '460px',
+  width: '105%',
+  marginLeft: '-10px'
+};
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <Segment style={slideShowStyle} textAlign="center">
+        <BackgroundSlideshow images={[ arcade, bball, boardGame, bowling, tugOfWar, xbox]} animationDelay={6000}/>
+      </Segment>
     </div>
   );
 };
