6f2b7972c24182ff4c79e85f90b699ee8537bbb5
Ryan Murry <ryanjmurry@gmail.com>
2018-10-03 17:32:24 -0700

    update signed out menu, add links to sign up and log in pages, add button to create pickup

--- src/features/nav/Menus/SignedOutMenu.jsx
+++ src/features/nav/Menus/SignedOutMenu.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Button, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+const menuStyles = {
+  marginRight: '15px'
+};
+
+const createButtonStyles ={
+  fontSize: '0.8em'
+}
 
 const SignedOutMenu = () => {
   return (
-    <Menu.Menu position="right">
-      <Menu.Item>Sign In</Menu.Item>
-      <Menu.Item>Sign Up</Menu.Item>
+    <Menu.Menu position="right" style={menuStyles}>
+      <Menu.Item style={createButtonStyles}>
+        <Button as={Link} to="/newpickup" basic size='mini'>
+          <Icon name="add" />
+          create pickup
+        </Button>
+      </Menu.Item>
+      <Menu.Item as={Link} to="/login">
+        log in
+      </Menu.Item>
+      <Menu.Item as={Link} to="/signup">
+        sign up
+      </Menu.Item>
     </Menu.Menu>
   );
 };
