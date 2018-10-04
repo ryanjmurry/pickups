73eef53cc6e3907aaee36d404ff3b4ac3c2c2e06
Ryan Murry <ryanjmurry@gmail.com>
2018-10-03 21:49:06 -0700

    import and position HowItWorks component

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Slideshow from './Slideshow';
 import Hook from './Hook';
+import HowItWorks from './HowItWorks';
 
 const HomePage = () => {
   return (
     <div>
-      <div style={{ position: 'relative', marginBottom: '-400px', zIndex: '-1'}}>
+      <div style={{ position: 'relative', marginBottom: '-400px', zIndex: '-1' }}>
         <Slideshow />
       </div>
       <div>
         <Hook />
+      </div>
+      <div style={{marginTop: '175px'}}>
+        <HowItWorks />
       </div>
     </div>
   );
