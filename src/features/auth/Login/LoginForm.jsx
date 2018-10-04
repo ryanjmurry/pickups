2262462d51c142bd540c848fd4d3debdf871b47b
Ryan Murry <ryanjmurry@gmail.com>
2018-10-03 23:11:44 -0700

    add styling, refactor to class component, move handle form submission out of render to handle redirect on success

--- src/features/auth/Login/LoginForm.jsx
+++ src/features/auth/Login/LoginForm.jsx
@@ -1,35 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { Field, reduxForm } from 'redux-form';
-import { Form, Segment, Button, Label } from 'semantic-ui-react';
+import { Form, Segment, Button, Label, Grid, Divider } from 'semantic-ui-react';
 import TextInput from '../../../app/common/form/TextInput';
 import { login, socialLogin } from '../authActions';
 import SocialLogin from '../SocialLogin/SocialLogin';
+
+const loginHeaderStyle = {
+  fontFamily: 'Righteous',
+  fontSize: '4em'
+};
 
 const actions = {
   login,
   socialLogin
 };
 
-const LoginForm = ({ login, handleSubmit, error, socialLogin }) => {
-  return (
-    <Form onSubmit={handleSubmit(login)}>
-      <Segment>
-        <Field name="email" type="text" component={TextInput} placeholder="Email Address" />
-        <Field name="password" type="password" component={TextInput} placeholder="Password" />
-        <div>
-          {error && (
-            <Label basic color="red">
-              {error}
-            </Label>
-          )}
-        </div>
-        <Button color="green">Login</Button>
-        <SocialLogin socialLogin={socialLogin} />
-      </Segment>
-    </Form>
-  );
-};
+class LoginForm extends Component {
+
+  handleFormSubmission = values => {
+    this.props.login(values);
+    this.props.history.push('/pickups')
+  }
+
+  render() {
+    const { error, socialLogin } = this.props
+    return (
+      <Grid centered columns={4}>
+      <Grid.Column>
+        <Form onSubmit={this.props.handleSubmit(this.handleFormSubmission)}>
+          <Segment.Group>
+            <Segment textAlign="center">
+              <h1 style={loginHeaderStyle}>log in</h1>
+              <h3>
+                not a user?{' '}
+                <span>
+                  <Link to="/signup" style={{ color: '#a333c8' }}>
+                    sign up
+                  </Link>
+                </span>
+              </h3>
+            </Segment>
+            <Segment padded>
+              <Field name="email" type="text" component={TextInput} placeholder="Email Address" />
+              <Field name="password" type="password" component={TextInput} placeholder="Password" />
+              <div>
+                {error && (
+                  <Label basic color="red">
+                    {error}
+                  </Label>
+                )}
+              </div>
+              <Button color="purple" style={{ fontSize: '1.25em' }}>
+                log in
+              </Button>
+            </Segment>
+            <Divider horizontal>Or</Divider>
+            <Segment basic>
+              <SocialLogin socialLogin={socialLogin} />
+            </Segment>
+          </Segment.Group>
+        </Form>
+      </Grid.Column>
+    </Grid>
+    )
+  }
+}
 
 export default connect(
   null,
