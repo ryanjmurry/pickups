af325fa3981858bc8fb6a9f1c62ddcbe5b975eea
Ryan Murry <ryanjmurry@gmail.com>
2018-10-04 04:05:08 -0700

    add dashboard slogan component and positioned

--- src/features/pickup/PickUpDashboard/PickUpDashboard.jsx
+++ src/features/pickup/PickUpDashboard/PickUpDashboard.jsx
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import PickupDashboardHeader from './PickupDashboardHeader';
+import PickupDashboardSlogan from './PickupDashboardSlogan';
 
 const mapState = state => ({
-  pickups: state.firestore.ordered.pickups
+  pickups: state.firestore.ordered.pickups,
+  auth: state.firebase.auth
 });
 
 class PickupDashboard extends Component {
   render() {
-    const { pickups } = this.props;
+    const { pickups, auth } = this.props;
     return (
       <div>
-        <PickupDashboardHeader />
+        <div style={{ position: 'relative', marginBottom: '-400px', zIndex: '-1' }}>
+          <PickupDashboardHeader />
+        </div>
+        <div style={{position: 'absolute', left: '250px', top: '220px'}}>
+          <PickupDashboardSlogan auth={auth}/>
+        </div >
+        <div style={{marginTop: '175px'}}/>
       </div>
     );
   }
