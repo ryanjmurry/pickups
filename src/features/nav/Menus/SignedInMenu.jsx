277c6ef608a1bd2ddb8dc126e51e793c31bf340c
Ryan Murry <ryanjmurry@gmail.com>
2018-10-03 21:12:28 -0700

    cleaned up formatting issues

--- src/features/nav/Menus/SignedInMenu.jsx
+++ src/features/nav/Menus/SignedInMenu.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import { Menu, Image, Dropdown, Button, Icon } from 'semantic-ui-react';
+import { Menu, Image, Dropdown, Button, Icon, Divider } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 const style = {
   marginRight: '15px'
 };
 
-const createButtonStyles ={
+const createButtonStyles = {
   fontSize: '0.8em'
-}
-
+};
 
 const SignedInMenu = ({ onLogOut, auth }) => {
   return (
     <Menu.Menu position="right">
       <Menu.Item style={createButtonStyles}>
-        <Button as={Link} to='/newpickup' basic>
+        <Button as={Link} to="/newpickup" basic>
           <Icon name="add" />
           create pickup
         </Button>
@@ -24,9 +23,9 @@ const SignedInMenu = ({ onLogOut, auth }) => {
         <Image avatar src={auth.photoURL || `https://api.adorable.io/avatars/80/${auth.uid}.png`} />
         <Dropdown pointing="top right">
           <Dropdown.Menu>
-            <Dropdown.Item as={Link} to='/profile' text="profile" icon="user circle" />
-            <Dropdown.Item  as={Link} to='/mypickups' text="my pickups" icon="trophy" />
-            <Dropdown.Item as={Link} to='/settings' text="settings" icon="setting" />
+            <Dropdown.Item as={Link} to="/profile" text="profile" icon="user circle" />
+            <Dropdown.Item as={Link} to="/mypickups" text="my pickups" icon="trophy" />
+            <Dropdown.Item as={Link} to="/settings" text="settings" icon="setting" />
             <Dropdown.Item onClick={onLogOut} text="log out" icon="sign out" />
           </Dropdown.Menu>
         </Dropdown>
