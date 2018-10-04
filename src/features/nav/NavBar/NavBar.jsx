8e7a431cb7f6adf1fc1612c6fce4a64ef86424a6
Ryan Murry <ryanjmurry@gmail.com>
2018-10-03 17:18:13 -0700

    refactor to class component, connect firebase and store, conditionally render signed in or signed out menu based on auth status, minor styling

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withFirebase } from 'react-redux-firebase';
+import { connect } from 'react-redux'
 import { Menu, Header } from 'semantic-ui-react';
 import SignedInMenu from '../Menus/SignedInMenu';
 import SignedOutMenu from '../Menus/SignedOutMenu';
 
 const logoText = {
   fontFamily: 'Righteous',
-  fontSize: '1.25em',
+  fontSize: '2em',
   color: 'black'
-}
-
-const navStyles = {
-  border: '1px solid black'
-}
-
-const NavBar = () => {
-  return (
-    <Menu size='massive' fixed="top" text style={navStyles}>
-      <Menu.Item>
-        <Header style={logoText}>p i c k u p s</Header>
-      </Menu.Item>
-      {/* <SignedInMenu /> */}
-      {/* <SignedOutMenu /> */}
-    </Menu>
-  );
 };
 
-export default NavBar;
+const navStyles = {
+  marginTop: '10px',
+  borderBottom: '1px solid black'
+};
+
+const mapState = state => ({
+  auth: state.firebase.auth
+})
+
+class NavBar extends Component {
+  handleLogOut = () => {
+    this.props.firebase.logout();
+  };
+  render() {
+    const { auth } = this.props
+    return (
+      <Menu size="huge" fixed="top" text style={navStyles}>
+        <Menu.Item>
+          <Header style={logoText}>p i c k u p s</Header>
+        </Menu.Item>
+        {!auth.isEmpty && <SignedInMenu onLogOut={this.handleLogOut} auth={auth}/>}
+        {auth.isEmpty && <SignedOutMenu />}
+      </Menu>
+    );
+  }
+}
+
+export default withFirebase(connect(mapState)(NavBar));
